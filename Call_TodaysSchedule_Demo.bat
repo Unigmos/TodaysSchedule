9ba856f799717b5b5e690454035526f76acfe1d3
Shaneron <77985354+Unigmos@users.noreply.github.com>
2021-12-12 19:57:52 +0900

    Add files via upload

---
+++ Call_TodaysSchedule_Demo.bat
@@ -0,0 +1,6 @@
+@echo off
+REM ディレクトリ移動:作業領域を指定
+cd c:\Users\ユーザー名\Desktop\My_Programs\TodaysSchedule
+
+REM pythonスクリプト実行:パスを通していれば「python TodaysSchedule.py」で実行可能
+C:\Users\ユーザー名\AppData\Local\Programs\Python\Python310\python.exe TodaysSchedule.py
