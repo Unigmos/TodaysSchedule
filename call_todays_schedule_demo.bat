14fe6a072190d2f8db0fd5c2ef338d43b5979ed3
Shaneron <77985354+Unigmos@users.noreply.github.com>
2021-12-18 11:12:13 +0900

    Update call_todays_schedule_demo.bat

--- call_todays_schedule_demo.bat
+++ call_todays_schedule_demo.bat
@@ -2,5 +2,5 @@
 REM ディレクトリ移動:作業領域を指定
 cd c:\Users\ユーザー名\Desktop\My_Programs\todays_schedule
 
-REM pythonスクリプト実行:パスを通していれば「python TodaysSchedule.py」で実行可能
+REM pythonスクリプト実行:パスを通していれば「python todays_schedule.py」で実行可能
 C:\Users\ユーザー名\AppData\Local\Programs\Python\Python310\python.exe todays_schedule.py
